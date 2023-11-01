883f28260fec7fd965f285f0f832bc80402b83bb
Kartikey Pandey <imkpandey31@gmail.com>
2023-11-01 18:38:36 +0530

    Added Lenis SmoothScroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import Navbar from "./components/Navbar";
 import TextAnimation from "./components/TextAnimation";
+import Lenis from "@studio-freight/lenis";
 
 import "./App.css";
 
 function App() {
+  const lenis = new Lenis();
+
+  lenis.on("scroll", (e) => {
+    console.log(e);
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
   return (
     <>
       <Navbar />
--- src/components/TextAnimation.jsx
+++ src/components/TextAnimation.jsx
@@ -1,8 +1,20 @@
 import { useEffect, useState } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import Lenis from "@studio-freight/lenis";
 
 const TextAnimation = () => {
+  //Lenis smoothscroll
+  const lenis = new Lenis();
+
+  lenis.on("scroll", ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+
   const [isHovering, setIsHovering] = useState(false);
   const [focusBackground, setFocusBackground] = useState("/focus.png");
   const [buttonBoxIndex, setButtonBoxIndex] = useState(-1);
